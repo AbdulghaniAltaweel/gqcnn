d60c8cd2cdf7961a4b9b35925534c3a6480a54d6
AbdulghaniAltaweel <abd_gt@yahoo.com>
2019-05-31 15:41:24 +0200

    comments

--- test_finetuning.py
+++ test_finetuning.py
@@ -32,6 +32,6 @@ gqcnn = get_gqcnn_model('tf')(gqcnn_params)
 trainer = get_gqcnn_trainer('tf')(gqcnn, 'data/training/Dexnet-2.0_testFinetuning', 'image_wise', 'models/', train_config, 'GQCNN-2.0_finetuned_org')
 # trainer = get_gqcnn_trainer('tf')(gqcnn, 'data/training/Dexnet-2.0_testTraining', 'image_wise', 'models/',train_config, 'GQCNN-2.0_Training_from_Scratch')
 
-#%%
+#%% Start finetuning 
 trainer.finetune('models/GQCNN-2.0_org')
 logger.info('Total Fine-tuning Time: ' + str(utils.get_elapsed_time(time.time() - start_time)))
